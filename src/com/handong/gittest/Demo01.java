8a11240ed7a8d2b2ba1945505ee16b1b10e7a717
handong <handong@bus365.com>
2020-05-26 16:18:28 +0800

    idea提交代码测试

---
+++ src/com/handong/gittest/Demo01.java
@@ -0,0 +1,12 @@
+package com.handong.gittest;
+
+/**
+ * @author handong
+ * @date 2020/5/26 - 16:15
+ */
+public class Demo01 {
+
+    public static void main(String[] args) {
+        System.out.println("Git ---- 测试代码");
+    }
+}
